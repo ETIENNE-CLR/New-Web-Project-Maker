cae31714e007ef55be4e14179bc7fc87d1c38ce0
Etienne Caulier <152507490+ETIENNE-CLR@users.noreply.github.com>
2026-01-22 13:59:10 +0100

    clean config index

--- www/src/base/config.php
+++ www/src/base/config.php
@@ -1,25 +1,13 @@
 <?php
+
 // Config
 date_default_timezone_set('Europe/Paris');
 require ROOT_PATH . 'src/base/env.php';
-
-// Dev mode
-function isDevMode(): bool
-{
-    return isset($_ENV['DEV_MOD']) && 'true' === $_ENV['DEV_MOD'];
-}
+require ROOT_PATH . 'src/base/funct.php';
 
 // Affichage des erreurs
 if (isDevMode()) {
     ini_set('display_errors', '1');
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
-}
-
-// Debug
-function Debug()
-{
-    if (!isDevMode()) return;
-    dump('Je suis passé par là');
-    die();
 }
---
+++ www/src/base/funct.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Méthode qui permet de dire si on est en mode developpeur ou non
+ * @return bool si on est en mode developpeur ou non
+ */
+function isDevMode(): bool
+{
+    return isset($_ENV['DEV_MOD']) && 'true' === $_ENV['DEV_MOD'];
+}
+
+/**
+ * Méthode qui aide au débuggage
+ */
+function Debug()
+{
+    if (!isDevMode()) return;
+    dump('Je suis passé par là');
+    die();
+}
