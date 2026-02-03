2134602b67bd3d282aa8f4fed0301fd656f7098c
Etienne Caulier <152507490+ETIENNE-CLR@users.noreply.github.com>
2026-02-03 09:26:22 +0100

    update config

--- www/src/base/config.php
+++ www/src/base/config.php
@@ -1,8 +1,16 @@
 <?php
 
+// Renforce le typage strict / fiabilité du code
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+
+// Charger les variables d'environnement depuis .env
+$dotenv = Dotenv::createImmutable(ROOT_PATH);
+$dotenv->load();
+
 // Config
 date_default_timezone_set('Europe/Paris');
-require ROOT_PATH . 'src/base/env.php';
 require ROOT_PATH . 'src/base/funct.php';
 
 // Affichage des erreurs
